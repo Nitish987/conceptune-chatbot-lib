d6f743a57b50b18e07aaf014ca2ac91dd41bd948
Nitish Kumar <nk98721000@gmail.com>
2023-12-24 13:44:56 +0530

    token authentication

--- chatbot.js
+++ chatbot.js
@@ -20,11 +20,9 @@ sendBtn.addEventListener('click', event => {
     if (message.value != '' || message .value != null) {
         const chat = buildMessage('user', message.value);
         messageHolder.appendChild(chat);
-        HttpClient.post(`/chatbot/v1/bot/?project_id=${Credentials.projectId}&api_id=${Credentials.apiId}`, {
+        HttpClient.get(`/chatbot/v1/bot/?project_id=${Credentials.projectId}&api_id=${Credentials.apiId}&query=${message.value}`, {
             "Authorization": Credentials.apiKey,
             "Content-Type": "application/json",
-        }, {
-            query: message.value
         }).then(res => {
             if (res.success) {
                 const chat = buildMessage('bot', res.data.answer);
--- common.js
+++ common.js
@@ -14,15 +14,41 @@ class HttpClient {
 
     constructor() {}
 
+    static async _base(method, path, headers, data) {
+        try {
+            const url = Credentials.baseUrl + path
+            const response = await fetch(url, {
+                method: method,
+                headers: headers,
+                credentials: 'include',
+                body: JSON.stringify(data)
+            });
+            return response.json();
+        } catch(e) {
+            return {
+                success: false
+            }
+        }
+    }
+
     static async post(path, headers, data) {
-        const url = Credentials.baseUrl + path
-        console.log(url)
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: headers,
-            credentials: 'include',
-            body: JSON.stringify(data)
-        });
-        return response.json();
+        return await this._base('POST', path, headers, data);
+    }
+
+    static async get(path, headers, data) {
+        return await this._base('GET', path, headers, data);
     }
 }
+
+class TokenAuthentication {
+    authPath = `/customer/v1/auth/?project_id=${Credentials.projectId}`;
+
+    constructor() {}
+
+    async authenticate() {
+        await HttpClient.get(this.authPath);
+    }
+}
+
+const tokenAuth = new TokenAuthentication();
+tokenAuth.authenticate();
